05f8bf627f0d2d7bdd51c3c155b880f5983e05c0
Hemakumar <hkakaraparti@sapient.com>
2020-09-15 11:06:11 +0530

    Updated config and html to use HLS format

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const NodeMediaServer = require('node-media-server');
- 
+
 const config = {
   rtmp: {
     port: 1935,
@@ -10,9 +10,28 @@ const config = {
   },
   http: {
     port: 8000,
+    mediaroot: './media',
     allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: '/usr/local/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        vc: "copy",
+        vcParam: [],
+        ac: "aac",
+        acParam: ['-ab', '64k', '-ac', '1', '-ar', '44100'],
+        rtmp:true,
+        rtmpApp:'live2',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
   }
 };
- 
+
 var nms = new NodeMediaServer(config)
 nms.run();
